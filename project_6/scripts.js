1132165b877cea157af4adbb8c68f250be911d43
Tanuj Garg <tanuj.adhomes@gmail.com>
2018-06-14 13:22:49 +0530

    project 10 complete

--- project_6/scripts.js
+++ project_6/scripts.js
@@ -1,14 +1,17 @@
 var locationSearch = "";
+var descriptionsearch =""
 function findjobs(){
     // read email by querySelector API
     let location = document.querySelector("#location").value;
     let description = document.querySelector("#description").value;
+    descriptionsearch = description;
+    locationSearch = location;
     
     if(!location || location === " ") alert("Please enter the location");
     else if(!description || description === " ") alert("Please enter job description");
     // query URL
     else{
-        var queryURL = `https://jobs.github.com/positions.json?description=${description}&location=${location}`
+        var queryURL = `https://us-central1-fir-testing-696e9.cloudfunctions.net/githubJobs/${location}/${description}`
     
     // make AJAX request using fetch API
     fetch(queryURL)
@@ -27,6 +30,7 @@ function findjobs(){
 
 function displayLocationResult(result) {
     // get message div
+    console.log(result);
     let msgDiv = document.querySelector("#message");
     // select result div
     let div = document.querySelector("#result");
@@ -41,30 +45,36 @@ function displayLocationResult(result) {
                                 <strong>oops!</strong> ${result.message} \
                             </div>`;    
     }
+    else if(result.length === 0) {
+        msgDiv.innerHTML = `<div class="alert alert-danger alert-dismissible" role="alert"> \
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button> \
+                                <h3>No jobs !!<h3> \
+                            </div>`;
+    }
     else {
         // display success
         msgDiv.innerHTML = `<div class="alert alert-success alert-dismissible" role="alert"> \
                                 <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button> \
-                                <strong>Weather of </strong><h3 style="text-transform: uppercase">${locationSearch}.<h3> \
+                                <h4>Jobs in ${locationSearch} of ${descriptionsearch}<h4> \
                             </div>`;
         
         // clear contents
         div.innerHTML = "";
 
-        const weather = result.consolidated_weather;
-        console.log(weather);
 
-        for(let i=0; i<weather.length; i++) {
-            console.log(weather[i].weather_state_name);
-            var elemts = `<div>${weather[i]}`;
+        for(let i=0; i<result.length; i++) {
             let hackedHTMLDiv = `<div class="jumbotron" id="id_${i}"> \
                                     <div class="row">
-                                    <h3>Date: <h5  style="color:green">${weather[i].applicable_date}</h5></h3>
-                                        <div class="col-xs-12 col-sm-12" style="text-align:center">
-                                            <h3>Minimum Temprature : <h5 style="color:blue">${weather[i].min_temp} &deg;C</a></h5></h3>
-                                            <h3>Maximum Temprature : <h5 style="color:blue">${weather[i].max_temp} &deg;C</h5></hh3>
-                                            <h3>Current Temprature : <h5 style="color:blue">${weather[i].the_temp} &deg;C</h5></h3>
-                                            <h3>Wind Speed: <h5 style="color:blue">${weather[i].wind_speed}</h5></h3>
+                                    <h3>Date: <h5  style="color:green"><a href="${result[i].company_url}">${result[i].created_at}</a></h5></h3>
+                                        <div class="col-xs-12 col-sm-6" style="text-align:center">
+                                            <img height="200px" width="200px" src="${result[i].company_logo}">
+                                            <h1>Company : <h2 style="color:blue">${result[i].company}</a></h5></h3>
+                                            <h3>Post : <h5 style="color:blue">${result[i].title}</a></h5></h3>
+                                            <h3>Job type : <h5 style="color:blue">${result[i].type}</h5></hh3>
+                                            <h3>Company location : <h5 style="color:blue">${result[i].location}</h5></h3>
+                                        </div>
+                                        <div>
+                                            <h3>Description: <h5 style="color:blue">${result[i].description}</h5></h3>
                                         </div>
                                     </div>
                                 </div>`;
